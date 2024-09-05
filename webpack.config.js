b535e125567aea72fadd8c4ebfe252bb2a19f52a
NicolasKonishi <145286206+NicolasKonishi@users.noreply.github.com>
2024-09-05 15:17:09 -0300

    Inicializando react no projeto

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,36 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
-const { Template } = require("webpack");
 
-module.exports ={
-        devtool: "source-map",
-        entry : "./src/index.js",
-        module: {
-            rules: [{
-                test: /\.js$/,
+module.exports = {
+    devtool: "source-map",
+    entry: "./src/index.js",
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/, 
                 exclude: /node_modules/,
-                use: { loader: "babel-loader"}
-         },
-         {
-            test: /\.html$/,
-            use: [{ loader: "html-loader"}]
-        },
-        {
-            test: /\.css$/,
-            use: ["style-loader" , "css-loader"]
-        },
-        {
-            test: /\.(png|jpe?g|gif)$/i,
-            use: ["file-loader"]
-        },
+                use: { loader: "babel-loader" }
+            },
+            {
+                test: /\.html$/,
+                use: [{ loader: "html-loader" }]
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: ["file-loader"]
+            }
         ]
     },
-    plugin : [
+    resolve: {
+        extensions: ['.js', '.jsx'] 
+    },
+    plugins: [
         new HtmlWebPackPlugin({
-            Template: './public/index.html'
+            template: './public/index.html'
         })
     ]
-}
+};
+
